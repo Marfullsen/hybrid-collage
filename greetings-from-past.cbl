42c5beae27f0b96db4480dbdd7f83f71a4800c49
Francisco Marfull <56993209+Marfullsen@users.noreply.github.com>
2019-10-25 20:33:14 -0300

    Create greetings-from-past.cbl
    My first source file was written in COBOL! 😦️

---
+++ greetings-from-past.cbl
@@ -0,0 +1,18 @@
+      ******************************************************************
+      * Author: @marfullsen
+      * Date: 25-oct-2019
+      * Purpose: Teaching cobol.
+      * Compiler: gnu-cobol
+      * Version 1.0, tested on linux.
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. GREETINGS-FROM-PAST.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "This message was written COBOL!!!"
+                    "Greetings from 1960 ;)"
+            STOP RUN.
+       END PROGRAM GREETINGS-FROM-PAST.
